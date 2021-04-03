5a9e373f2604020884d2b4e5368db284cc7d5d45
JGoodHub <jagoodmail@gmail.com>
2021-04-03 11:16:18 +0100

    [Main Menu] Added ability to launch first level from main menu

---
+++ Assets/5-Scripts/SceneLauncher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLauncher : MonoBehaviour
+{
+
+    /// <summary>
+    /// Launches the scene with the specific index in the build settings
+    /// </summary>
+    /// <param name="sceneIndex">Scene index to launch</param>
+    public void LaunchSceneWithIndex(int sceneIndex)
+    {
+        Debug.Assert(sceneIndex >= 0);
+        Debug.Assert(sceneIndex < SceneManager.sceneCountInBuildSettings);
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+
+
+}
