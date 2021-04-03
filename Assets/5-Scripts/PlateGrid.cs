51a2a84afe6c1f2959c2a162a73e36be555742d6
JGoodHub <jagoodmail@gmail.com>
2021-04-03 11:14:56 +0100

    [Tile Generation] Created scripts to auto generate a random grid of tiles and their backing plates given a width, height and spacing parameter

---
+++ Assets/5-Scripts/PlateGrid.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateGrid : MonoBehaviour
+{
+
+    [Header("Board Parameters")]
+
+    public int width;
+    public int height;
+    public float spacing;
+
+    public GameObject platePrefab;
+    public Transform plateParent;
+    private GameObject[,] plateObjects;
+
+    private void OnValidate()
+    {
+        width = Mathf.Clamp(width, 3, 9);
+        height = Mathf.Clamp(height, 3, 9);
+
+        spacing = Mathf.Clamp(spacing, 0f, float.MaxValue);
+    }
+
+
+    private void Start()
+    {
+        plateObjects = new GameObject[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                GameObject plateObject = Instantiate(platePrefab, Vector3.zero, Quaternion.identity, plateParent);
+                plateObject.transform.localPosition = new Vector3(x - (width / 2) + (width % 2 == 0 ? 0.5f : 0), y - (height / 2) + (height % 2 == 0 ? 0.5f : 0), 0f);
+                plateObject.transform.localPosition *= spacing;
+
+                plateObjects[x, y] = plateObject;
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+
+                Vector3 gizmoCente = plateParent.transform.position;
+                gizmoCente += new Vector3(x - (width / 2) + (width % 2 == 0 ? 0.5f : 0), y - (height / 2) + (height % 2 == 0 ? 0.5f : 0), 0f) * spacing;
+
+                Gizmos.DrawWireCube(gizmoCente, new Vector3(spacing, spacing, 0));
+            }
+        }
+    }
+
+
+}
---
+++ Assets/5-Scripts/TileGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    [Header("Board Parameters")]
+
+    public int width;
+    public int height;
+    public float spacing;
+
+    public int seed;
+
+    public GameObject[] tilePrefabs;
+    public Transform tilesParent;
+    private TileData[,] tileGrid;
+
+    private void OnValidate()
+    {
+        width = Mathf.Clamp(width, 3, 9);
+        height = Mathf.Clamp(height, 3, 9);
+
+        spacing = Mathf.Clamp(spacing, 0f, float.MaxValue);
+    }
+
+    private void Start()
+    {
+        Random.InitState(seed);
+
+        // Clear any existing tiles
+
+        // Create the tile objects and place then on the grid
+
+        tileGrid = new TileData[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                TileData tileData = Instantiate(tilePrefabs[Random.Range(0, tilePrefabs.Length)], Vector3.zero, Quaternion.identity, tilesParent).GetComponent<TileData>();
+                tileData.transform.localPosition = new Vector3(x - (width / 2) + (width % 2 == 0 ? 0.5f : 0), y - (height / 2) + (height % 2 == 0 ? 0.5f : 0), 0f);
+                tileData.transform.localPosition *= spacing;
+
+                tileGrid[x, y] = tileData;
+            }
+        }
+
+        // Set the adjacents for each tile for use later
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int yOff = -1; yOff < 3; yOff++)
+                {
+                    for (int xOff = -1; xOff < 3; xOff++)
+                    {
+                        if (y + yOff >= 0 && y + yOff < height && x + xOff >= 0 && x + xOff < width)
+                        {
+                            tileGrid[x, y].adjacents.Add(tileGrid[x + xOff, y + yOff]);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Vector3 gizmoCente = tilesParent.transform.position;
+                gizmoCente += new Vector3(x - (width / 2) + (width % 2 == 0 ? 0.5f : 0), y - (height / 2) + (height % 2 == 0 ? 0.5f : 0), 0f) * spacing;
+
+                Gizmos.DrawWireSphere(gizmoCente, spacing / 2f);
+            }
+        }
+    }
+
+}
